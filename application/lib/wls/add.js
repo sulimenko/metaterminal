a751b5b071bc43027d45c1757eda875ee689ff5b
alexey <sulimenko@ptfin.kz>
2023-09-19 12:24:37 +0600

    update added

--- application/lib/wls/add.js
+++ application/lib/wls/add.js
@@ -2,7 +2,8 @@
 async ({ login, name, instrument }) => {
   const wl = await db.pg.select('terminal_wls', ['name', 'symbol', 'source', 'order'], { login, name }).order('order');
   // console.log(wl);
-  const find = wl.find((each) => each.symbol === instrument.symbol && each.source === instrument.exchange);
+  // const find = wl.find((each) => each.symbol === instrument.symbol && each.source === instrument.source);
+  const find = wl.find((each) => each.symbol === instrument.symbol);
 
   if (find !== undefined) return false;
   await db.pg.insert('terminal_wls', {
