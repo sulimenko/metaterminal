d9560386db9d67ee343bf380387efc38ab327f2d
alexey <sulimenko@ptfin.kz>
2023-09-19 11:33:56 +0600

    add delete WL

--- application/lib/wls/delete.js
+++ application/lib/wls/delete.js
@@ -2,9 +2,19 @@
 async ({ login, name, instrument }) => {
   const wl = await db.pg.select('terminal_wls', ['id', 'name', 'symbol', 'source', 'order'], { login, name }).order('order');
   // console.log(wl);
-  const find = wl.find((each) => each.symbol === instrument.symbol && each.source === instrument.exchange);
+  const find = wl.find((each) => each.symbol === instrument.symbol && each.source === instrument.source);
 
   if (find === undefined) return false;
   await db.pg.delete('terminal_wls', { id: find.id });
+
+  let i = 1;
+  for (const each of wl) {
+    if (each.order !== i) {
+      each.order = i;
+      await db.pg.update('terminal_wls', { order: each.order }, { id: each.id });
+    }
+    i++;
+  }
+
   return true;
 };
