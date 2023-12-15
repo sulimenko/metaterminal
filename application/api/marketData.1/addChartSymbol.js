580a4c939469de868d24df1dcdc5538a6eaa6680
alexey <sulimenko@ptfin.kz>
2023-12-15 18:14:20 +0600

    bugfix addChartSymbol

--- application/api/marketData.1/addChartSymbol.js
+++ application/api/marketData.1/addChartSymbol.js
@@ -3,5 +3,6 @@
   method: async ({ instrument, period, limit }) => {
     // console.log('addChartSymbol: ', instrument, period, limit);
     return lib.marketData.addChart({ instrument, userId: context.client.session.state.user_id, period, limit });
+    // return { chart: { full: [], last: {} } };
   },
 });
