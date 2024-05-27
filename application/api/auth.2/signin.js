2453bcaafca3d4f07b29667a9cc7cdb976f2849d
alexey <sulimenko@ptfin.kz>
2024-05-27 13:58:00 +0500

    add user type

--- application/api/auth.2/signin.js
+++ application/api/auth.2/signin.js
@@ -10,14 +10,14 @@
       const refreshToken = await api.auth.provider.getRefreshToken(user.login);
       return { status: 'external', text: user.type, refreshToken };
     }
-    const { user_id, password: hash } = user;
+    const { user_id, password: hash, type } = user;
     const valid = await metarhia.metautil.validatePassword(password, hash);
     // if (!valid) throw new Error('Incorrect login or password');
     // console.log(login, password, user, hash, valid);
     if (!valid) return { status: 'unlogged', text: 'Incorrect login or password', token: null };
     console.log(`Logged user: ${login}`);
     const token = api.auth.provider.generateToken();
-    const data = { user_id, login };
+    const data = { user_id, login, type };
     context.client.startSession(token, data);
     const { ip } = context.client;
     await api.auth.provider.createSession(token, data, { ip, login });
--- application/api/auth.2/userData.js
+++ application/api/auth.2/userData.js
@@ -2,6 +2,6 @@
   access: 'private',
   method: async () => {
     // const user = await api.auth.provider.getUser(login);
-    return { id: context.session.state.user_id, login: context.session.state.login };
+    return { id: context.session.state.user_id, login: context.session.state.login, type: context.session.state.type };
   },
 });
