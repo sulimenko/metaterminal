02e3d3c0ad5745048fb759012bc0533e4ff88b71
alexey <sulimenko@ptfin.kz>
2024-06-21 13:24:28 +0500

    update new user

--- application/api/ptfin.1/accounts.js
+++ application/api/ptfin.1/accounts.js
@@ -1,9 +1,7 @@
 ({
   access: 'private',
   method: async () => {
-    // console.log(context.session.state.user_id);
     const accounts = await lib.ptfin.accounts({ userId: context.session.state.user_id });
-    // console.warn(typeof accounts, Object.keys(accounts));
     context.session.state.accounts = Object.keys(accounts);
     return accounts;
   },
--- application/lib/settings/get.js
+++ application/lib/settings/get.js
@@ -6,16 +6,16 @@ async ({ login, type }) => {
 
   await db.pg.insert('terminal_settings', {
     login,
-    update_time: 5,
-    themes: 'dark',
-    chart_type: 'candle_solid',
-    chart_timeframe: '900',
-    chart_main: '',
-    chart_sub: 'VOL',
-    main: JSON.stringify({ theme: 'dark', refresh: 5 }),
-    chart: JSON.stringify({ sub: 'VOL', main: '', type: 'candle_solid', timeframe: 900 }),
+    // update_time: 5,
+    // themes: 'dark',
+    // chart_type: 'candle_solid',
+    // chart_timeframe: '3600',
+    // chart_main: '',
+    // chart_sub: 'VOL',
+    main: JSON.stringify({ theme: 'dark', symbol: null, refresh: '10', account: null, confirmation: true }),
+    chart: JSON.stringify({ sub: 'VOL', main: [], type: 'candle_solid', timeframe: 3600 }),
     created_at: new Date(),
     updated_at: new Date(),
   });
-  return lib.settings.get({ login });
+  return lib.settings.get({ login, type });
 };
