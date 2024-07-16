d577cfda9ee9c9a11874361f83ddefa9b0d0b18d
alexey <sulimenko@ptfin.kz>
2024-07-16 15:37:46 +0500

    bugfix documents

--- application/api/user.1/documents.js
+++ application/api/user.1/documents.js
@@ -1,11 +1,6 @@
 ({
   access: 'private',
-  method: async () => {
-    // console.log(context.session.state.user_id);
-    const documents = await lib.ptfin.documents({ id: context.session.state.user_id });
-    // console.warn(typeof accounts, Object.keys(accounts));
-    // context.session.documents.documents = Object.keys(documents);
-    console.log('metaterminal documents :', documents);
-    return documents;
+  method: async ({ id }) => {
+    return lib.ptfin.documents({ id });
   },
 });
--- application/lib/ptfin/documents.js
+++ application/lib/ptfin/documents.js
@@ -1,8 +1,8 @@
 async ({ id }) => {
   if (!id) return [];
   const method = 'data/documents';
-  const data = { user: [id] };
-  console.log('metaterminal :', data);
+  const data = { ids: [id] };
+  // console.log('metaterminal :', data);
 
   return lib.ptfin.sendPost({ method, data });
 };
