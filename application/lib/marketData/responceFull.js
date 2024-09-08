1b724d247fa831c0ea06134487b153604d556959
alexey <sulimenko@ptfin.kz>
2024-09-08 15:17:14 +0500

    bugfix charts

--- application/lib/marketData/responceFull.js
+++ application/lib/marketData/responceFull.js
@@ -1,12 +1,12 @@
 async (chart, end) => {
-  const waitChart = async (result, chart, end, interval = 200) => {
+  const waitChart = async (result, chart, end, period = 200) => {
     while (new Date().getTime() <= end) {
       if (chart.data.full.length > 0) {
         result.chart = JSON.parse(JSON.stringify(chart.data));
         return result;
       }
       // console.log('responceFull', chart.symbol, new Date().getTime(), end, chart.data);
-      await lib.utils.wait(interval);
+      await lib.utils.wait(period);
     }
     return result;
   };
