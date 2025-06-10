fbdf148f204c89256cdbf495cb503ee851050c9d
alexey <sulimenko@ptfin.kz>
2025-06-10 18:58:18 +0500

    bugfix small

--- application/lib/marketData/optionChart.js
+++ application/lib/marketData/optionChart.js
@@ -3,8 +3,12 @@ async ({ instrument, period, limit }) => {
   const method = 'marketdata/barcharts';
   const data = { symbol: lib.utils.makeOptSymbol(instrument.symbol), period, limit: 100 };
 
+  // todo: only Get, update to stream
   const response = await lib.ts.sendPost({ method, data });
-  if (response.result.Bars.length === 0) return { error: true, text: 'No data for this period' };
+  if (response === 'error') return { error: true, text: 'error sendPost' };
+  if (response.result === undefined || response.result.Bars === undefined || response.result.Bars.length === 0) {
+    return { error: true, text: 'No data for this period' };
+  }
 
   response.result.Bars.sort((a, b) => a.Epoch - b.Epoch);
   const chart = domain.marketData.charts.getChart({ instrument, period });
