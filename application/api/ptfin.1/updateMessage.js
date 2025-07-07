40b3f524750661545f9aed9bd83fc4a4d8532120
alexey <sulimenko@ptfin.kz>
2025-07-07 11:58:03 +0500

    update sendMessage

--- application/api/ptfin.1/updateMessage.js
+++ application/api/ptfin.1/updateMessage.js
@@ -2,9 +2,8 @@
   access: 'public',
   method: async (data) => {
     // console.warn(data);
-    if (data.type === 'order') {
-      lib.ptfin.updateOrder({ update: data });
-    }
-    return 'OK';
+    if (data.type === 'order') return lib.ptfin.updateOrder({ update: data });
+    if (data.type === 'strategy') return lib.analytics.newSelection({ update: data });
+    return ['OK'];
   },
 });
--- application/lib/analytics/newSelection.js
+++ application/lib/analytics/newSelection.js
@@ -1,6 +1,6 @@
 async ({ update }) => {
   for (const client of domain.clients.terminal.getByAccount({ account: update.account })) {
-    client.emit('ptfin/updateOrder', update);
+    client.emit('analytics/newSelection', update);
   }
   return ['OK'];
 };
