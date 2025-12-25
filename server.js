6f40ba9a60cce5ffe3c9d2ffe9c6213cb9e873e5
Egor Ivanov <e.ivanov@m2trust.de>
2025-12-25 07:57:00 -0500

    revert server js

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'development' && process.execArgv.length > 0) {
-  // Strip execArgv to avoid invalid flags being forwarded to worker threads.
-  process.execArgv.splice(0, process.execArgv.length);
-}
-
-
 require('impress');
