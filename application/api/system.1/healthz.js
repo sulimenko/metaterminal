10913b01944d375533f0323984b019aec81d40c6
Egor Ivanov <e.ivanov@m2trust.de>
2026-01-03 20:16:19 -0500

    version for healthz

--- application/api/system.1/healthz.js
+++ application/api/system.1/healthz.js
@@ -2,6 +2,7 @@
   access: 'public',
   method: async () => ({
     status: 'ok',
+    version: require('../../../package.json').version,
     uptime: process.uptime(),
     timestamp: new Date().toISOString(),
   }),
