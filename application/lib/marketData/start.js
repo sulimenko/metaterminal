04934ba2413e847147ce9e8028876780df51b3a8
Egor Ivanov <e.ivanov@m2trust.de>
2026-01-03 20:36:05 -0500

    prettier run

--- application/lib/marketData/start.js
+++ application/lib/marketData/start.js
@@ -1,13 +1,16 @@
 async () => {
   if (application.worker.id === 'W1') {
     console.info('start lib.marketData');
-    setInterval(() => {
-      // console.info('start lib.marketData:', Object.keys(domain.marketData.quotes.values));
-      const list = [];
-      for (const symbol of Object.keys(domain.marketData.quotes.values)) {
-        if (domain.marketData.quotes.values[symbol].source === 'TN') list.push({ symbol });
-      }
-      if (list.length > 0) lib.marketData.requestQuote({ instruments: list });
-    }, 3 * 60 * 1000);
+    setInterval(
+      () => {
+        // console.info('start lib.marketData:', Object.keys(domain.marketData.quotes.values));
+        const list = [];
+        for (const symbol of Object.keys(domain.marketData.quotes.values)) {
+          if (domain.marketData.quotes.values[symbol].source === 'TN') list.push({ symbol });
+        }
+        if (list.length > 0) lib.marketData.requestQuote({ instruments: list });
+      },
+      3 * 60 * 1000,
+    );
   }
 };
