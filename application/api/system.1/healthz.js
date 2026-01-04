a1e6c6061c8fa98cc9905013f1991e233719032c
Egor Ivanov <e.ivanov@m2trust.de>
2026-01-03 20:22:08 -0500

    require change

--- application/api/system.1/healthz.js
+++ application/api/system.1/healthz.js
@@ -1,15 +1,23 @@
 ({
   access: 'public',
   method: async () => {
-    let buildInfo = {};
-    try {
-      buildInfo = require('../../../build-info.json');
-    } catch {
-      buildInfo = {};
-    }
+    const readJson = (filePath) => {
+      try {
+        const data = node.fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+      } catch {
+        return {};
+      }
+    };
 
-    const version =
-      buildInfo.version || require('../../../package.json').version;
+    const rootPath = process.cwd();
+    const buildInfoPath = node.path.join(rootPath, 'build-info.json');
+    const packagePath = node.path.join(rootPath, 'package.json');
+
+    const buildInfo = readJson(buildInfoPath);
+    const packageInfo = readJson(packagePath);
+
+    const version = buildInfo.version || packageInfo.version;
 
     return {
       status: 'ok',
